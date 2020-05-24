b469cf04eaaedb2d5e8f12619cb446739e84fa70
Pedro <pedroaugustoribasp@gmail.com>
2020-05-24 18:18:59 -0300

    feature development

--- js/script.js
+++ js/script.js
@@ -6,19 +6,31 @@ let arrayPeople = [];
 let countFoundPeople = null;
 
 let tabListPeople = null;
-
+let tabSumaryPeople = null;
+let numberFormat = null;
+const prevebtFormSubmit = ()=>{
+    const form = document.querySelector('form');
+    form.addEventListener('submit',(event)=>{
+        event.preventDefault();
+    })
+}   
 window.addEventListener('load',()=>{
+    // Inputs
     inputName = document.querySelector('#search-bar');
     btnPesquisa = document.querySelector('#btn-search');
+    // Tabs
     tabListPeople = document.querySelector('#tabPeoples');
-    countFoundPeople= document.querySelector('#countPeople');
+    tabSumaryPeople = document.querySelector('#tabSumary');
+    // Count
+    countFoundPeople = document.querySelector('#countPeople');
+    numberFormat = Intl.NumberFormat('pt-br',{ minimumFractionDigits: 0, maximumFractionDigits: 2 });
 
     prevebtFormSubmit();
-    insertPeople();
+    getPeopleApi();
     searchPerson();
 });
 
-async function insertPeople(){
+async function getPeopleApi(){
     const data  = await fetch('https://randomuser.me/api/?seed=javascript&results=100&nat=BR&noinfo');
     const json = await data.json();
     arrayPeople = json.results.map((person)=>{
@@ -33,7 +45,6 @@ async function insertPeople(){
 }
 
 function searchPerson(){
-
     inputName.addEventListener('keyup',(event)=>{             
         if(event){
 
@@ -52,20 +63,33 @@ function renderPeopleList(char){
     const peopleFoundArray = [];
     let count = 0;
     arrayPeople.forEach((person)=>{
-       if(person.name.startsWith(char)){
-        peopleFoundArray.push(person);
-        ++count;
-       }
+        if(char.length>1){   
+            if(person.name.includes(char)){
+              peopleFoundArray.push(person);
+              ++count;
+            }
+        }else{
+            if(person.name.startsWith(char)){
+                peopleFoundArray.push(person);
+                ++count;
+              }
+        }
+    
     });
+
     countFoundPeople.textContent = count;
+    buildHtmlList(peopleFoundArray)
+    renderSumary(peopleFoundArray);
+}
+function buildHtmlList(peopleFoundArray){
     let listPeopleHtml = '<div>';
     peopleFoundArray.forEach((person)=>{
         const {name,age,picture} = person;
 
         const html =`
-            <div style="display: flex; flex-direction: row; align-items: center;">
+            <div class="person">
                 <div> 
-                    <img style="border-radius: 50%;width: 50px;height: 50px; margin-right: 10px; margin-left: 10px;" src="${picture}" alt="${name}">
+                    <img src="${picture}" alt="${name}">
                 </div> 
                 <div>
                    <ul>
@@ -81,9 +105,41 @@ function renderPeopleList(char){
     tabListPeople.innerHTML = listPeopleHtml;
 }
 
-const prevebtFormSubmit = ()=>{
-    const form = document.querySelector('form');
-    form.addEventListener('submit',(event)=>{
-        event.preventDefault();
-    })
+
+function renderSumary(peopleFoundArray){
+    let countMen = 0;
+    let countWoman = 0;
+    let totalAge = 0;
+    let midle = 0;
+    peopleFoundArray.forEach((person)=>{    
+        if(person.gender ==='female'){
+            ++countWoman;
+        }else{
+            ++countMen;
+        }
+    });
+    totalAge = peopleFoundArray.reduce((acc,curr)=>acc+curr.age,0);    
+    midle =FormatNumber(totalAge/(countMen+countWoman)) ;
+   
+    buildSumaryHtml(countWoman,countMen,totalAge,midle);
+}
+
+function buildSumaryHtml(countWoman,countMen,totalAge,midle){
+    let sumaryHtml = '<div>';
+    const html =`
+            <div>
+               <ul>
+                <li>${countWoman}</li>
+                <li>${countMen}</li>
+                <li>${totalAge}</li>
+                <li>${midle}</li>
+               </ul>
+            </div>`;
+    sumaryHtml += html;
+    sumaryHtml += '</div>';
+    tabSumaryPeople.innerHTML = sumaryHtml;
+}
+
+function FormatNumber(number){
+    return numberFormat.format(number);
 }
